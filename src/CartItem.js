b46d6bdd37d4a5ac399b2379f95f6d9ec6f4faf2
ankush789 <ankushsharma.4771@gmail.com>
2020-10-27 00:47:46 +0530

    Added a function to increase quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,8 +9,15 @@ class CartItem extends React.Component {
           qty: 1,
           img: ''
         }
+        // 2nd way to bind this
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    // third way is to use arrow functions
+    increaseQuantity= () => {
+        console.log(this.state);
     }
     render(){
+        //Desconstructing object
         const { title, price, qty } = this.state;
         return(
             <div className="cart-item">
@@ -22,9 +29,14 @@ class CartItem extends React.Component {
                     <div style = {{ color: '#777' }}>Rs. { price }</div>
                     <div style = {{ color:'#777' }}>Qty: { qty }</div>
                     <div className="cart-item-actions">
-                        <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/992/992651.svg" />
-                        <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/svg/1665/1665612.svg" />
-                        <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
+                        <img alt="decrease" className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/992/992651.svg" 
+                        // onClick={this.increaseQuantity.bind(this) }/>
+                        onClick = {this.increaseQuantity} />
+                        <img alt="increase" className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/1665/1665612.svg" />
+                        <img alt="delete" className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/1214/1214428.svg" />
                     </div>
                 </div>
             </div>
